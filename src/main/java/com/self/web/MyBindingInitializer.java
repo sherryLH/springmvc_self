d530b3eddc249ff8f81efc45ea9a390b889f1f7a
lixiao <lixiao@eeepay.cn>
2018-04-26 18:00:02 +0800

    添加自定义属性

---
+++ src/main/java/com/self/web/MyBindingInitializer.java
@@ -0,0 +1,23 @@
+package com.self.web;
+/**
+ * 自定义属性编辑器
+ */
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.support.WebBindingInitializer;
+import org.springframework.web.context.request.WebRequest;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MyBindingInitializer implements WebBindingInitializer {
+
+    public void initBinder(WebDataBinder binder, WebRequest request){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        dateFormat.setLenient(false);
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
+        binder.registerCustomEditor(String.class, new StringTrimmerEditor(false));
+    }
+}
